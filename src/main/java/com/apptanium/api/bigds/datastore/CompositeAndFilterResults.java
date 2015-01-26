bfb17cb2d6004f04d98d0ef2e588134f59d85c1f
Saurabh Gupta <saurabhgupta.sg@gmail.com>
2015-01-26 09:40:05 -0500

    signature change for constructor, todo items recorded

--- src/main/java/com/apptanium/api/bigds/datastore/CompositeAndFilterResults.java
+++ src/main/java/com/apptanium/api/bigds/datastore/CompositeAndFilterResults.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -19,32 +20,47 @@ import java.util.List;
  */
 public class CompositeAndFilterResults extends QueryResults {
 
-  private final List<Scan> scans;
+  private final List<List<Scan>> dnfScans;
+  private final List<Scan> rangeScans;
   private final Table indexTable;
-  private final ResultScanner firstScanner;
-  private final Iterator<Result> firstResultIterator;
+  private final List<ResultScanner> resultScanners = new ArrayList<>();
+  private final List<Iterator<Result>> resultIterators = new ArrayList<>();
 
   CompositeAndFilterResults(DatastoreService datastoreService,
                             Query query,
-                            List<Scan> scans,
+                            List<List<Scan>> dnfScans,
+                            List<Scan> rangeScans,
                             Table indexTable) throws IOException {
     super(datastoreService, query);
-    this.scans = scans;
+    this.dnfScans = dnfScans;
+    this.rangeScans = rangeScans;
     this.indexTable = indexTable;
-    this.firstScanner = indexTable.getScanner(this.scans.get(0));
-    this.firstResultIterator = this.firstScanner.iterator();
+    for (Scan rangeScan : rangeScans) {
+      ResultScanner scanner = this.indexTable.getScanner(rangeScan);
+      resultScanners.add(scanner);
+      resultIterators.add(scanner.iterator());
+    }
+    //todo: build set of iterators that control the top iteration cycle
   }
 
   @Override
   public boolean hasNext() {
-    return this.firstResultIterator.hasNext();
+    boolean hasNext = true;
+    for (Iterator<Result> resultIterator : resultIterators) {
+      hasNext &= resultIterator.hasNext();
+    }
+    return hasNext;
   }
 
   @Override
   public Entity next() {
+    //todo: implement simultaneous iterations across all relevant iterators
+    //todo: implement tests for scans that are not part of the top level iterators
+/*
     Result result = firstResultIterator.next();
     byte[] parentBytes = result.getValue(DatastoreConstants.ID_COLUMN_FAMILY_BYTES, DatastoreConstants.ID_COLUMN_PARENT_BYTES);
     byte[] keyIdBytes = result.getValue(DatastoreConstants.ID_COLUMN_FAMILY_BYTES, DatastoreConstants.ID_COLUMN_KEY_BYTES);
+*/
 
     return null;
   }
